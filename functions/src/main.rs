a0403a4ac45f2c4b108103b8e3a6178c8fdf90b0
Eric <ekiesow@mail.csuchico.edu>
2020-10-21 23:38:09 -0700

    reorganized, added comments and code

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_variables)]
 fn main() {
     println!("Hello, world!");
 
@@ -6,43 +7,59 @@ fn main() {
     let y = {
         let x = 3;
         x + 1 // expressions do not include ending semic
-    }; // block that evaluates to 4 :0
+    }; // block that evaluates to 4
 
     let x = five();
     println!("The value of x from five() is: {}", x);
+
+    let x = six();
+    println!("The value of x from six() is: {}", x);
+
     let x = plus_one(x);
     println!("The value of x after plus_one() is: {}", x);
 }
 
+// Rust convention to use snake case for function (and variable) names
+// In funtion signatures, you must declare the type of each parameter
 fn another_function(x: i32, y: i32) {
     println!("Another function printing x: {}", x);
     println!("Another function printing y: {}", y);
 }
 
-// functions return the last expression implicitly
-fn five() -> i32 {
-    5
-}
-
-fn plus_one(x: i32) -> i32 {
-    x + 1 //; this would be a statement and produce a compiler error
-}
-
 /*
  * Statements: instructions that perform some action and do not return a value
- * let y = 6; // does not return a value, x = y = 6 does not work
- *
+ * 
+ * let y = 6; // does not return a value
+ * let x = (let y = 6); // does not work as let keyword does not return a value
+ * function definitions are also statements
  */
 
 /*
  * Expressions: evaluate to a resulting value
  * do not end in a semicolon, semic will turn expression into statement
  *
- * function definitions
  * 5 + 6 // expression that evaluates to 11
  * 6 in let y = 6 is an expression that evaluates to 6
  * calling a function is an expression
  * calling a macro is an expression
  * block used to create new scopes, {}, is an expression
- *
  */
+
+ /* functions with return values*/
+
+// functions return the last expression implicitly
+fn five() -> i32 {
+    5
+}
+
+// use the return keyword to return early
+fn six() -> i32 {
+    if true {
+        return 6
+    }
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1 //; <-- this  would be a statement and produce a compiler error
+}
